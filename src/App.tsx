a2df78eb07cbffaece2bccc1f7ed6edfa1238ce2
podestal <luisrodriguezpodesta@gmail.com>
2024-11-05 19:01:58 -0500

    dark light mode basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import Button from "./components/ui/Button"
 
 const App = () => {
 
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.querySelector('html')?.classList.add('dark')
+    } else {
+      document.querySelector('html')?.classList.remove('dark')
+    }
+  }, [theme])
+
+  const handleChangeTheme = () => {
+    console.log('changing theme')
+    
+    setTheme( prev => prev === 'light' ? 'dark' : 'light')
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="h-screen flex flex-col justify-center items-center text-3xl font-bold dark:bg-slate-950 dark:text-slate-50">
+      <h2 className="text-5xl text-center">This is a text</h2>
+      <p className="text-xs w-[40%] text-center my-6 dark:text-slate-400 text-neutral-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, labore. Itaque sunt molestiae pariatur unde obcaecati sapiente ea in consequuntur, eligendi, harum facere voluptate! Provident est recusandae ex commodi voluptatem.</p>
+      <Button 
+        label="Theme"
+        onClick={handleChangeTheme}
+      />
     </h1>
   )
 }
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,68 @@
+import { RiLoader2Fill } from "@remixicon/react"
+import { useEffect, useState } from "react"
+
+interface Props {
+    label: string
+    loading?: boolean
+    disable?: boolean
+    color?: keyof typeof colors
+    onClick?: () => void
+}
+
+const colors = {
+    blue: {
+        enabled: 'bg-blue-700 hover:bg-blue-600 text-slate-50',
+        disabled: 'bg-blue-800 text-slate-400 cursor-not-allowed',
+    },
+    red: {
+        enabled: 'bg-red-700 hover:bg-red-600 text-slate-50',
+        disabled: 'bg-red-800 text-slate-400 cursor-not-allowed',
+    },
+    green: {
+        enabled: 'bg-green-600 hover:bg-green-500 text-slate-50',
+        disabled: 'bg-green-700 text-slate-400 cursor-not-allowed',
+    },
+    amber: {
+        enabled: 'bg-amber-600 hover:bg-amber-500 text-slate-50',
+        disabled: 'bg-amber-700 text-slate-400 cursor-not-allowed',
+    }
+}
+
+const Button = ({
+    label,
+    loading=false,
+    disable=false,
+    color='blue',
+    onClick
+}: Props) => {
+
+    const [disabled, setDisabled] = useState(false)
+    const pickedColor = colors[color]
+
+    useEffect(() => {
+        
+        
+        disable || loading ? setDisabled(true) : setDisabled(false)        
+    }, [disable, loading])
+
+  return (
+    <button 
+        disabled={disabled}
+        onClick={onClick}
+        className={`
+            ${disabled === true ? pickedColor.disabled : pickedColor.enabled}  
+            py-2 px-4 text-sm rounded-md  mx-auto text-center mt-6`}>
+        {loading 
+        ? 
+        <div className="flex justify-center items-center gap-2">
+            <p>Loading</p> 
+            <RiLoader2Fill className="animate-spin"/> 
+        </div>
+        : 
+        <p>{label}</p>
+        }
+    </button>
+  )
+}
+
+export default Button
