7586ab1da338e87cbd21de0809ad8240747f85d1
podestal <luisrodriguezpodesta@gmail.com>
2024-11-08 13:11:29 -0500

    get Cart hook

---
+++ src/hooks/api/cart/useGetCart.ts
@@ -0,0 +1,15 @@
+import { UseQueryResult, useQuery } from "@tanstack/react-query"
+import getCartService, {Cart} from "../../../services/api/cartService"
+import { getCacheCartKey } from "../../../utils/keys"
+
+const useGetCart = (access?: string): UseQueryResult<Cart, Error> => {
+    const cartService = getCartService(access)
+    const CACHE_CART_KEY = getCacheCartKey(access)
+    return useQuery({
+        queryKey: CACHE_CART_KEY,
+        queryFn: () => cartService.get(access),
+        staleTime: 1 * 60 * 1000
+    })
+}
+
+export default useGetCart
--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,3 +1,7 @@
 export const CATEGORIES_CACHE_KEY = ['categories']
 
 export const DISHES_CACHE_KEY = ['dishes']
+
+export const getCacheCartKey = (access?: string) => {
+    return access ? ['authenticated cart'] : ['anonymous cart']
+} 
