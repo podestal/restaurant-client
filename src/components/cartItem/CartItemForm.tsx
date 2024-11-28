96ae26dc38536c175e3111a57d3f81b509fc89dc
podestal <luisrodriguezpodesta@gmail.com>
2024-11-28 07:08:06 -0500

    cart item form done

---
+++ src/components/cartItem/CartItemForm.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react"
+import Button from "../ui/Button"
+import Modal from "../ui/Modal"
+import TextArea from "../ui/TextArea"
+import { UseMutationResult } from "@tanstack/react-query"
+import { CartItem } from "../../services/api/cartItemService"
+import { CreateCartItemData } from "../../hooks/api/cartItem/useCreateCartItem"
+import { Dish } from "../../services/api/dishService"
+import useNotificationsStore from "../../hooks/store/useNotificationsStore"
+
+interface Props {
+    dish: Dish
+    open: boolean
+    cartId: number
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+    createCartItem?: UseMutationResult<CartItem, Error, CreateCartItemData>
+    count?: number
+    setCount?: React.Dispatch<React.SetStateAction<number>>
+    cartItem?: CartItem
+}
+
+const CartItemForm = ({ dish, open, cartId, setOpen, createCartItem, count, setCount, cartItem }: Props) => {
+
+    const [observations, setObservations] = useState('')
+    const { setShow, setType, setMessage } = useNotificationsStore()
+
+    const handleCreateOrderItem = () => {
+
+        const quantity = count ? count : 0
+        const price = dish.cost * quantity
+
+        createCartItem && createCartItem.mutate({ cartItem: {
+            quantity,
+            dish: dish.id,
+            price,
+            cart: cartId,
+            observations,
+        }}, {
+            onSuccess: () => {
+                setShow(true)
+                setType('success')
+                setMessage('Item added to cart')
+                setCount && setCount(0)
+                setOpen(false)
+            },
+            onError: (error) => {
+                setShow(true)
+                setType('error')
+                setMessage(`Error: ${error.message}`)
+            }
+        })
+    }
+
+  return (
+    <Modal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+    >
+        <div className="flex flex-col justify-start items-center mx-auto gap-12 w-[75%]">
+            <h2 className="text-2xl font-palanquin">Any obervation to add ...</h2>
+            <TextArea 
+                placeholder="Observations ..."
+                value={observations}
+                onChange={ e => setObservations(e.target.value)}
+            />
+            <Button 
+                onClick={handleCreateOrderItem}
+                label="Add to cart"
+            />
+        </div>
+    </Modal>
+  )
+}
+
+export default CartItemForm
--- src/components/cartItem/CreateCartItem.tsx
+++ src/components/cartItem/CreateCartItem.tsx
@@ -4,8 +4,7 @@ import useNotificationsStore from "../../hooks/store/useNotificationsStore"
 import { Cart } from "../../services/api/cartService"
 import { Dish } from "../../services/api/dishService"
 import Button from "../ui/Button"
-import Modal from "../ui/Modal"
-import TextArea from "../ui/TextArea"
+import CartItemForm from "./CartItemForm"
 
 interface Props {
     cart: Cart
@@ -19,31 +18,6 @@ const CreateCartItem = ({ cart, dish, count, setCount }: Props) => {
     const createCartItem = useCreateCartItem(cart.id)
     const { setShow, setType, setMessage } = useNotificationsStore()
     const [ open, setOpen ] = useState(false)
-    const [observations, setObservations] = useState('')
-
-    const handleCreateOrderItem = () => {
-
-        createCartItem.mutate({ cartItem: {
-            quantity: count,
-            dish: dish.id,
-            price: dish.cost * count,
-            cart: cart.id,
-            observations,
-        }}, {
-            onSuccess: () => {
-                setShow(true)
-                setType('success')
-                setMessage('Item added to cart')
-                setCount(0)
-                setOpen(false)
-            },
-            onError: (error) => {
-                setShow(true)
-                setType('error')
-                setMessage(`Error: ${error.message}`)
-            }
-        })
-    }
 
   return (
     <div>
@@ -59,23 +33,15 @@ const CreateCartItem = ({ cart, dish, count, setCount }: Props) => {
             }
             label="Add"
         />
-        <Modal
-            isOpen={open}
-            onClose={() => setOpen(false)}
-        >
-            <div className="flex flex-col justify-start items-center mx-auto gap-12 w-[75%]">
-                <h2 className="text-2xl font-palanquin">Any obervation to add ...</h2>
-                <TextArea 
-                    placeholder="Observations ..."
-                    value={observations}
-                    onChange={ e => setObservations(e.target.value)}
-                />
-                <Button 
-                    onClick={handleCreateOrderItem}
-                    label="Add to cart"
-                />
-            </div>
-        </Modal>
+        <CartItemForm 
+            dish={dish}
+            open={open}
+            setOpen={setOpen}
+            count={count}
+            setCount={setCount}
+            cartId={cart.id}
+            createCartItem={createCartItem}
+        />
     </div>
   )
 }
