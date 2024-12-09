4297e053d9b20b26a119a2f2996a4d54385befe8
podestal <luisrodriguezpodesta@gmail.com>
2024-12-09 11:43:05 -0500

    promotion card

--- src/components/promotion/PromotionCard.tsx
+++ src/components/promotion/PromotionCard.tsx
@@ -6,9 +6,10 @@ interface Props {
 
 const PromotionCard = ({ promotion }: Props) => {
   return (
-    <div className="w-full">
-        <h2 className="text-2xl">{promotion.name}</h2>
-        <p>{promotion.description}</p>
+    <div className="w-full flex flex-col justify-start items-center h-[140px] border-2 border-slate-200 dark:border-slate-900 rounded-2xl shadow-xl shadow-slate-700 dark:shadow-slate-700 relative">
+        <h2 className="text-2xl mt-6 text-blue-700 font-bold mb-2">{promotion.name}</h2>
+        <p className="text-sm dark:text-slate-300 mx-6">{promotion.description}</p>
+        <div className={`absolute top-0 left-6 w-[20px] h-full ${promotion.is_active ? 'bg-green-600' : 'dark:bg-neutral-500 bg-neutral-300'}`}/>
     </div>
   )
 }
--- src/components/promotion/Promotions.tsx
+++ src/components/promotion/Promotions.tsx
@@ -17,12 +17,14 @@ const Promotions = () => {
   return (
     <div>
         <CreatePromotion />
-        {promotions.map( promotion => (
-            <PromotionCard 
-                key={promotion.id}
-                promotion={promotion}
-            />
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-12">
+            {promotions.map( promotion => (
+                <PromotionCard 
+                    key={promotion.id}
+                    promotion={promotion}
+                />
+            ))}
+        </div>
     </div>
   )
 }
