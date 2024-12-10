764ec085d70db4a3ea7987a0bdd637e2ca16e32f
podestal <luisrodriguezpodesta@gmail.com>
2024-12-10 12:07:23 -0500

    create & update dish with discount

--- src/components/dish/DishCard.tsx
+++ src/components/dish/DishCard.tsx
@@ -19,8 +19,8 @@ const DishCard = ({ dish, cart }: Props) => {
         // viewport={{ margin: '-50%' }}
         className="flex flex-col max-md:mx-6 justify-start border-2 border-slate-200 dark:border-slate-900 px-4 rounded-2xl shadow-xl shadow-slate-700 dark:shadow-slate-700">
         <div className="relative mb-4">
-        {dish.discount && <div className="absolute top-0 left-0 w-full h-full">
-            <span className="absolute -top-0 left-3 w-14 h-10 bg-green-500 flex justify-center items-center">
+        {dish.discount > 0 && <div className="absolute top-0 left-0 w-full h-full">
+            <span className="absolute -top-0 left-0 w-14 h-10 bg-green-500 flex justify-center items-center">
                 <p className="text-sm font-bold font-poppins text-slate-50">Offer</p>
             </span>
         </div>}
@@ -30,7 +30,7 @@ const DishCard = ({ dish, cart }: Props) => {
         <div className="flex justify-between items-start mb-4 md:h-[80px] gap-6">
             <h2 className="text-3xl font-semibold font-palanquin">{dish.name}</h2>
             <div>
-                {dish.discount && <p className="font-bold mt-2 line-through dark:text-slate-400 text-slate-600">$.{dish.cost}</p>}
+                {dish.discount > 0 && <p className="font-bold mt-2 line-through dark:text-slate-400 text-slate-600">$.{dish.cost}</p>}
                 <p className="font-bold mt-2">$.{(dish.final_price).toFixed(2)}</p>
             </div>
         </div>
--- src/components/dish/DishCardAdmin.tsx
+++ src/components/dish/DishCardAdmin.tsx
@@ -23,7 +23,10 @@ const DishCardAdmin = ({ dish }: Props) => {
           <img src={dish.picture_url} alt={dish.picture_url} className="w-[200px] h-[100px] object-cover shadow-xl shadow-slate-500" />
           <p className="col-span-2 font-poppins font-bold">{dish.name}</p>
           <p className="col-span-2 dark:text-slate-300 text-slate-800 max-lg:hidden">{dish.description.length > 35 ? `${dish.description.slice(0, 35)} ...` : dish.description}</p>
-          <p className="font-poppins font-bold max-lg:hidden">{dish.final_price}</p>
+          <div className="flex flex-col justify-center items-center max-lg:hidden gap-4">
+            {dish.discount > 0 && <p className="font-poppins font-bold line-through dark:text-slate-400 text-slate-600">{dish.cost}</p>}
+            <p className="font-poppins font-bold">{(dish.final_price).toFixed(2)}</p>
+          </div>
           <p className={`shadow-xl ${dish.available ? 'text-green-700 bg-green-300 border-green-400 shadow-green-700' : 'text-red-700 bg-red-300 border-red-400 shadow-red-700'} text-center text-xs rounded-3xl py-2 border-2 w-full max-lg:w-[50%]`}>{dish.available ? 'Available' : 'Unavailable'}</p>
       </motion.div>
       <div className="h-full my-auto">
