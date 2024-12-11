6cc0d72cb9254c66df02ae914264787c517e6aa9
podestal <luisrodriguezpodesta@gmail.com>
2024-12-11 12:53:24 -0500

    ui for promotion card store

--- src/components/promotion/PromotionStoreCard.tsx
+++ src/components/promotion/PromotionStoreCard.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import { Promotion } from '../../services/api/promotionService'
 import { useState } from 'react'
 import ItemCounter from '../cart/ItemCounter'
+import Button from '../ui/Button'
 
 interface Props {
     promotion: Promotion
@@ -15,7 +16,7 @@ const PromotionStoreCard = ({ promotion }: Props) => {
     <motion.div 
         className="flex flex-col max-md:mx-6 justify-start border-2 border-slate-200 dark:border-slate-900 px-4 rounded-2xl shadow-xl shadow-slate-700 dark:shadow-slate-700">
         <div className="relative mb-4">
-            <div className="w-full h-[120px]" />
+            <div className="w-full h-[50px]" />
         </div>
 
         <div className="flex justify-between items-start mb-4 md:h-[80px] gap-6">
@@ -26,18 +27,18 @@ const PromotionStoreCard = ({ promotion }: Props) => {
         </div>
         <p className="text-xs dark:text-slate-400 text-slate-700">{promotion.description}</p>
         {/* <div className="w-full flex justify-between items-center mx-auto max-lg:my-10 lg:py-6">
-            <CreateCartItem 
-                cart={cart}
-                dish={dish}
-                count={count}
-                setCount={setCount}
-            />
+
 
         </div> */}
+        <div className="w-full flex justify-between items-center mx-auto max-lg:my-10 lg:py-6">
+        <Button 
+                label='Add'
+            />
             <ItemCounter 
                 counter={count}
                 setCounter={setCount}
             />
+        </div>
     </motion.div>
   )
 }
