1c714c10a5058f1b75a1c9d04751cb61389caaa0
podestal <luisrodriguezpodesta@gmail.com>
2024-12-13 12:32:13 -0500

    ticket from ticket generator

--- src/components/bill/Playground.tsx
+++ src/components/bill/Playground.tsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import Button from "../ui/Button"
+import { generateTicketData } from "../../utils/billing"
 
 const Playground = () => {
 
@@ -256,146 +257,12 @@ const Playground = () => {
         }
     }`
         
-
-
-
-    const ticket = {
-        personaId: "675c4d5b40264100151a3492",
-        personaToken: "DEV_ARMXKt1dLYTkhbI6bhp1ErGVimApMLB8CayiMsvjDulEWYFK7lUpLIKN4kAdWHsX",
-        fileName: "20482674828-03-B001-00000002",
-        documentBody: {
-          "cbc:UBLVersionID": { _text: "2.1" },
-          "cbc:CustomizationID": { _text: "2.0" },
-          "cbc:ID": { _text: "B001-00000002" },
-          "cbc:IssueDate": { _text: "2024-12-13" },
-          "cbc:IssueTime": { _text: "10:32:13" },
-          "cbc:InvoiceTypeCode": {
-            _attributes: { listID: "0101" },
-            _text: "03",
-          },
-          "cbc:Note": [
-            {
-              _text: "SESENTA Y SEIS CON 08/100 SOLES",
-              _attributes: { languageLocaleID: "1000" },
-            },
-          ],
-          "cbc:DocumentCurrencyCode": { _text: "PEN" },
-          "cac:AccountingSupplierParty": {
-            "cac:Party": {
-              "cac:PartyIdentification": {
-                "cbc:ID": { _attributes: { schemeID: "6" }, _text: "20482674828" },
-              },
-              "cac:PartyName": { "cbc:Name": { _text: "Axios" } },
-              "cac:PartyLegalEntity": {
-                "cbc:RegistrationName": { _text: "Axios" },
-                "cac:RegistrationAddress": {
-                  "cbc:AddressTypeCode": { _text: "0000" },
-                  "cac:AddressLine": { "cbc:Line": { _text: "217 primera" } },
-                },
-              },
-            },
-          },
-          "cac:AccountingCustomerParty": {
-            "cac:Party": {
-              "cac:PartyIdentification": {
-                "cbc:ID": { _attributes: { schemeID: "1" }, _text: "00000000" },
-              },
-              "cac:PartyLegalEntity": { "cbc:RegistrationName": { _text: "---" } },
-            },
-          },
-          "cac:TaxTotal": {
-            "cbc:TaxAmount": { _attributes: { currencyID: "PEN" }, _text: 10.08 },
-            "cac:TaxSubtotal": [
-              {
-                "cbc:TaxableAmount": { _attributes: { currencyID: "PEN" }, _text: 56 },
-                "cbc:TaxAmount": { _attributes: { currencyID: "PEN" }, _text: 10.08 },
-                "cac:TaxCategory": {
-                  "cac:TaxScheme": {
-                    "cbc:ID": { _text: "1000" },
-                    "cbc:Name": { _text: "IGV" },
-                    "cbc:TaxTypeCode": { _text: "VAT" },
-                  },
-                },
-              },
-            ],
-          },
-          "cac:LegalMonetaryTotal": {
-            "cbc:LineExtensionAmount": {
-              _attributes: { currencyID: "PEN" },
-              _text: 56,
-            },
-            "cbc:TaxInclusiveAmount": {
-              _attributes: { currencyID: "PEN" },
-              _text: 66.08,
-            },
-            "cbc:PayableAmount": {
-              _attributes: { currencyID: "PEN" },
-              _text: 66.08,
-            },
-          },
-          "cac:InvoiceLine": [
-            {
-              "cbc:ID": { _text: 1 },
-              "cbc:InvoicedQuantity": {
-                _attributes: { unitCode: "NIU" },
-                _text: 1,
-              },
-              "cbc:LineExtensionAmount": {
-                _attributes: { currencyID: "PEN" },
-                _text: 56,
-              },
-              "cac:PricingReference": {
-                "cac:AlternativeConditionPrice": {
-                  "cbc:PriceAmount": {
-                    _attributes: { currencyID: "PEN" },
-                    _text: 66.08,
-                  },
-                  "cbc:PriceTypeCode": { _text: "01" },
-                },
-              },
-              "cac:TaxTotal": {
-                "cbc:TaxAmount": {
-                  _attributes: { currencyID: "PEN" },
-                  _text: 10.08,
-                },
-                "cac:TaxSubtotal": [
-                  {
-                    "cbc:TaxableAmount": {
-                      _attributes: { currencyID: "PEN" },
-                      _text: 56,
-                    },
-                    "cbc:TaxAmount": {
-                      _attributes: { currencyID: "PEN" },
-                      _text: 10.08,
-                    },
-                    "cac:TaxCategory": {
-                      "cbc:Percent": { _text: 18 },
-                      "cbc:TaxExemptionReasonCode": { _text: "10" },
-                      "cac:TaxScheme": {
-                        "cbc:ID": { _text: "1000" },
-                        "cbc:Name": { _text: "IGV" },
-                        "cbc:TaxTypeCode": { _text: "VAT" },
-                      },
-                    },
-                  },
-                ],
-              },
-              "cac:Item": { "cbc:Description": { _text: "jhkhg jhg j" } },
-              "cac:Price": {
-                "cbc:PriceAmount": {
-                  _attributes: { currencyID: "PEN" },
-                  _text: 56,
-                },
-              },
-            },
-          ],
-        },
-      };
+    const ticket = generateTicketData({ amount: 100 })
       
     const invoiceObj = JSON.parse(invoice)
 
     const handleSunat = () => {
-        axios.post('https://back.apisunat.com/personas/v1/sendBill', JSON.parse(invoice), {
+        axios.post('https://back.apisunat.com/personas/v1/sendBill', ticket, {
             headers: {
             //   Authorization: `JWT DEV_ARMXKt1dLYTkhbI6bhp1ErGVimApMLB8CayiMsvjDulEWYFK7lUpLIKN4kAdWHsX`,
               "Content-Type": "application/json",
---
+++ src/utils/billing.ts
@@ -0,0 +1,188 @@
+import moment from "moment"
+
+const numberToWords = (amount: number) => {
+    const units = [
+        "", "UNO", "DOS", "TRES", "CUATRO", "CINCO", "SEIS", "SIETE", "OCHO", "NUEVE", "DIEZ", 
+        "ONCE", "DOCE", "TRECE", "CATORCE", "QUINCE", "DIECISÉIS", "DIECISIETE", "DIECIOCHO", "DIECINUEVE"
+    ]
+    const tens = ["", "", "VEINTE", "TREINTA", "CUARENTA", "CINCUENTA", "SESENTA", "SETENTA", "OCHENTA", "NOVENTA"]
+    const hundreds = [
+        "", "CIENTO", "DOSCIENTOS", "TRESCIENTOS", "CUATROCIENTOS", "QUINIENTOS", 
+        "SEISCIENTOS", "SETECIENTOS", "OCHOCIENTOS", "NOVECIENTOS"
+    ]
+    
+    const [integerPart, decimalPart] = amount.toFixed(2).split(".")
+
+    const convertInteger = (num: number): string => {
+        if (num === 0) return "CERO"
+        if (num === 100) return "CIEN"
+        if (num < 20) return units[num]
+        if (num < 100) return tens[Math.floor(num / 10)] + (num % 10 !== 0 ? " Y " + units[num % 10] : "")
+        if (num < 1000) {
+            return hundreds[Math.floor(num / 100)] + (num % 100 !== 0 ? " " + convertInteger(num % 100) : "")
+        }
+        if (num < 1000000) {
+            const thousands = Math.floor(num / 1000)
+            const remainder = num % 1000
+            return (thousands === 1 ? "MIL" : convertInteger(thousands) + " MIL") + (remainder !== 0 ? " " + convertInteger(remainder) : "")
+        }
+
+        return "CANTIDAD MUY ALTA"
+    };
+
+    const words = `${convertInteger(Number(integerPart))} CON ${decimalPart}/100 SOLES`
+    return words
+}
+
+// console.log(numberToWords(123456.78));
+// // Output: "CIENTO VEINTITRÉS MIL CUATROCIENTOS CINCUENTA Y SEIS CON 78/100 SOLES"
+
+// console.log(numberToWords(66.08));
+// // Output: "SESENTA Y SEIS CON 08/100 SOLES"
+
+interface TicketProps {
+    amount: number
+}
+
+export const generateTicketData = ({
+    amount,
+}: TicketProps) => {
+
+    const taxes = parseFloat((amount*0.18).toFixed(2))
+    const total = parseFloat((amount + taxes).toFixed(2))
+
+    const ticket = {
+        personaId: import.meta.env.VITE_PERSONAL_ID,
+        personaToken: import.meta.env.VITE_PERSONAL_TOKEN,
+        fileName: "20482674828-03-B001-00000004",
+        documentBody: {
+          "cbc:UBLVersionID": { _text: "2.1" },
+          "cbc:CustomizationID": { _text: "2.0" },
+          "cbc:ID": { _text: "B001-00000004" },
+          "cbc:IssueDate": { _text:  moment().format("YYYY-MM-DD") },
+          "cbc:IssueTime": { _text: moment().format("HH:mm:ss") },
+          "cbc:InvoiceTypeCode": {
+            _attributes: { listID: "0101" },
+            _text: "03",
+          },
+          "cbc:Note": [
+            {
+              _text: numberToWords(total),
+              _attributes: { languageLocaleID: "1000" },
+            },
+          ],
+          "cbc:DocumentCurrencyCode": { _text: "PEN" },
+          "cac:AccountingSupplierParty": {
+            "cac:Party": {
+              "cac:PartyIdentification": {
+                "cbc:ID": { _attributes: { schemeID: "6" }, _text: "20482674828" },
+              },
+              "cac:PartyName": { "cbc:Name": { _text: "Axios" } },
+              "cac:PartyLegalEntity": {
+                "cbc:RegistrationName": { _text: "Axios" },
+                "cac:RegistrationAddress": {
+                  "cbc:AddressTypeCode": { _text: "0000" },
+                  "cac:AddressLine": { "cbc:Line": { _text: "217 primera" } },
+                },
+              },
+            },
+          },
+          "cac:AccountingCustomerParty": {
+            "cac:Party": {
+              "cac:PartyIdentification": {
+                "cbc:ID": { _attributes: { schemeID: "1" }, _text: "00000000" },
+              },
+              "cac:PartyLegalEntity": { "cbc:RegistrationName": { _text: "---" } },
+            },
+          },
+          "cac:TaxTotal": {
+            "cbc:TaxAmount": { _attributes: { currencyID: "PEN" }, _text: taxes },
+            "cac:TaxSubtotal": [
+              {
+                "cbc:TaxableAmount": { _attributes: { currencyID: "PEN" }, _text: amount },
+                "cbc:TaxAmount": { _attributes: { currencyID: "PEN" }, _text: taxes },
+                "cac:TaxCategory": {
+                  "cac:TaxScheme": {
+                    "cbc:ID": { _text: "1000" },
+                    "cbc:Name": { _text: "IGV" },
+                    "cbc:TaxTypeCode": { _text: "VAT" },
+                  },
+                },
+              },
+            ],
+          },
+          "cac:LegalMonetaryTotal": {
+            "cbc:LineExtensionAmount": {
+              _attributes: { currencyID: "PEN" },
+              _text: amount,
+            },
+            "cbc:TaxInclusiveAmount": {
+              _attributes: { currencyID: "PEN" },
+              _text: total,
+            },
+            "cbc:PayableAmount": {
+              _attributes: { currencyID: "PEN" },
+              _text: total,
+            },
+          },
+          "cac:InvoiceLine": [
+            {
+              "cbc:ID": { _text: 1 },
+              "cbc:InvoicedQuantity": {
+                _attributes: { unitCode: "NIU" },
+                _text: 1,
+              },
+              "cbc:LineExtensionAmount": {
+                _attributes: { currencyID: "PEN" },
+                _text: amount,
+              },
+              "cac:PricingReference": {
+                "cac:AlternativeConditionPrice": {
+                  "cbc:PriceAmount": {
+                    _attributes: { currencyID: "PEN" },
+                    _text: total,
+                  },
+                  "cbc:PriceTypeCode": { _text: "01" },
+                },
+              },
+              "cac:TaxTotal": {
+                "cbc:TaxAmount": {
+                  _attributes: { currencyID: "PEN" },
+                  _text: taxes,
+                },
+                "cac:TaxSubtotal": [
+                  {
+                    "cbc:TaxableAmount": {
+                      _attributes: { currencyID: "PEN" },
+                      _text: amount,
+                    },
+                    "cbc:TaxAmount": {
+                      _attributes: { currencyID: "PEN" },
+                      _text: taxes,
+                    },
+                    "cac:TaxCategory": {
+                      "cbc:Percent": { _text: 18 },
+                      "cbc:TaxExemptionReasonCode": { _text: "10" },
+                      "cac:TaxScheme": {
+                        "cbc:ID": { _text: "1000" },
+                        "cbc:Name": { _text: "IGV" },
+                        "cbc:TaxTypeCode": { _text: "VAT" },
+                      },
+                    },
+                  },
+                ],
+              },
+              "cac:Item": { "cbc:Description": { _text: "From billing util" } },
+              "cac:Price": {
+                "cbc:PriceAmount": {
+                  _attributes: { currencyID: "PEN" },
+                  _text: amount,
+                },
+              },
+            },
+          ],
+        },
+      }
+
+    return ticket
+}
