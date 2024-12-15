825c2e109fcd444ef0390cce250f05e39b24499a
podestal <luisrodriguezpodesta@gmail.com>
2024-12-15 09:55:07 -0500

    totals in bill

--- src/components/bill/BillTotal.tsx
+++ src/components/bill/BillTotal.tsx
@@ -6,15 +6,28 @@ interface Props {
 
 const BillTotal = ({ orderItems }: Props) => {
 
-    const total =  orderItems.reduce((accumulator, orderItem) => {
+
+    const subTotal =  orderItems.reduce((accumulator, orderItem) => {
         return accumulator += orderItem.cost * orderItem.quantity
     }, 0)
+    const taxes = (subTotal * 0.18).toFixed(2)
+    const total = (subTotal * 1.18).toFixed(2)
 
   return (
-    <div className="w-full my-4 border-t-2 border-slate-4   00">
-        <div className="w-full flex justify-between items-start pt-4">
-            <p className="font-bold font-poppins text-lg">Total:</p>
-            {total && <p>{total.toFixed(2)}</p>}
+    <div className="w-full my-4 border-t-2 border-slate-4">
+        <div className="w-full flex flex-col justify-between items-start pt-4 gap-3">
+            <div className="w-full flex justify-between">
+                <p className="font-bold font-poppins text-lg">Sub Total:</p>
+                {subTotal && <p>{subTotal.toFixed(2)}</p>}
+            </div>
+            <div className="w-full flex justify-between">
+                <p className="font-bold font-poppins text-lg">Taxes:</p>
+                {taxes && <p>{taxes}</p>}
+            </div>
+            <div className="w-full flex justify-between"> 
+                <p className="font-bold font-poppins text-lg">Total:</p>
+                {total && <p className="font-bold">{total}</p>}
+            </div>
         </div>
     </div>
   )
