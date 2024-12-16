b0f6e76a37d30ade1f35249c402c59e12072c1f3
podestal <luisrodriguezpodesta@gmail.com>
2024-12-16 14:00:29 -0500

    faq effects

--- src/components/landing/FaqCard.tsx
+++ src/components/landing/FaqCard.tsx
@@ -1,15 +1,28 @@
 import { FAQ } from "./FrequentQuestions"
+import {motion} from 'framer-motion'
 
 interface Props {
     item: FAQ
 }
 
 const FaqCard = ({ item }: Props) => {
+
+    const variants = {
+        hidden: { opacity: 0, x: 50 }, 
+        visible: { opacity: 1, x: 0 }, 
+      }
+
   return (
-    <div className="w-full bg-slate-200 dark:bg-slate-800 px-6 py-4 rounded-2xl flex flex-col gap-6">
+    <motion.div 
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: 0.3 }}
+        transition={{ duration: 0.8 }}
+        variants={variants}
+        className="w-full bg-slate-200 dark:bg-slate-800 px-6 py-4 rounded-2xl flex flex-col gap-6">
         <h2 className="text-2xl font-bold">{item.question}</h2>
         <p>{item.answer}</p>
-    </div>
+    </motion.div>
   )
 }
 
