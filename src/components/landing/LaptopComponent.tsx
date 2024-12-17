a7964e03276cc706d6abe7ae3e05001756b0b39a
podestal <luisrodriguezpodesta@gmail.com>
2024-12-17 12:22:54 -0500

    promotions animation

--- src/components/landing/LaptopComponent.tsx
+++ src/components/landing/LaptopComponent.tsx
@@ -17,7 +17,7 @@ const RotatingModel = () => {
       <primitive 
         ref={modelRef} 
         object={useGLTF(laptop).scene} 
-        scale={0.4} 
+        scale={0.6} 
         position={[0, -0.5, 0]}
       />
     );
--- src/components/promotion/PromotionsStore.tsx
+++ src/components/promotion/PromotionsStore.tsx
@@ -1,6 +1,7 @@
 import useGetPromotion from "../../hooks/api/promotion/useGetPromotions"
 import useLoader from "../../hooks/ui/useLoader"
 import PromotionStoreCard from "./PromotionStoreCard"
+import { motion } from "framer-motion"
 
 const PromotionsStore = () => {
 
@@ -13,12 +14,17 @@ const PromotionsStore = () => {
     if (isSuccess)
 
   return (
-    <>
+    <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+    >
         <h2
             className="text-5xl font-bold font-poppins text-blue-700"
             id="promotions"
         >Promotions</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-12">
+        <div 
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-12">
             {promotions
             .filter( promotion => promotion.is_active)
             .map(promotion => (
@@ -28,7 +34,7 @@ const PromotionsStore = () => {
                 />
             ))}
         </div>
-    </>
+    </motion.div>
   )
 }
 
