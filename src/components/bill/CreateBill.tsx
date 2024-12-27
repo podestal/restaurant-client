546db024e3d52c044bc29b215be86d6da817c920
podestal <luisrodriguezpodesta@gmail.com>
2024-12-27 11:55:14 -0500

    order and bill handling loading error

--- src/components/bill/CreateBill.tsx
+++ src/components/bill/CreateBill.tsx
@@ -2,6 +2,8 @@ import useCreateBill from "../../hooks/api/bill/useCreateBill"
 import Button from "../ui/Button"
 import useAuthStore from "../../hooks/store/useAuthStore"
 import useLanguageStore from "../../hooks/store/useLanguageStore"
+import useNotificationsStore from "../../hooks/store/useNotificationsStore"
+import { useState } from "react"
 
 interface Props {
     tableId: number
@@ -13,17 +15,38 @@ const CreateBill = ({ tableId, setEnableCreateOrder }: Props) => {
     const access = useAuthStore(s => s.access) || ''
     const createBill = useCreateBill({ tableId })
     const lan = useLanguageStore(s => s.lan)
+    const { setShow, setType, setMessage } = useNotificationsStore()
+    const [loading, setLoading] = useState(false)
 
     const handleCreate = () => {
-        createBill.mutate({ bill: {}, access }, {onSuccess: () => setEnableCreateOrder(true)})
+        setLoading(true)
+        createBill.mutate({ bill: {}, access }, 
+            {
+                onSuccess: () => setEnableCreateOrder(true),
+                onError: err => {
+                    setShow(true)
+                    setType('error')
+                    setMessage(`Error: ${err}`)
+                },
+                onSettled: () => setLoading(false)
+            
+            })
     }
 
   return (
     <div className="w-full flex justify-center">
+        {loading 
+        ?
+        <Button 
+            label={lan === 'EN' ? 'Opening' : 'Inicando'}
+            disable={true}
+        />
+        : 
         <Button 
             label={lan === 'EN' ? 'Open Table' : 'Abrir Mesa'}
             onClick={handleCreate}
         />
+        }
     </div>
   )
 }
--- src/components/orders/CreateOrder.tsx
+++ src/components/orders/CreateOrder.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import useCreateOrder from "../../hooks/api/order/useCreateOrder"
 import useAuthStore from "../../hooks/store/useAuthStore"
 import useLanguageStore from "../../hooks/store/useLanguageStore"
 import Button from "../ui/Button"
+import useNotificationsStore from "../../hooks/store/useNotificationsStore"
 
 interface Props {
     tableId?: number
@@ -12,24 +14,43 @@ interface Props {
 
 const CreateOrder = ({ tableId, orderType='I', status='P', cart }: Props) => {
 
+    const { setShow, setType, setMessage } = useNotificationsStore()
     const userId = useAuthStore(s => s.userId)
     const access = useAuthStore(s => s.access) || ''
     const createOrder = useCreateOrder({ tableId, cart })
     const table = tableId ? tableId : null
     const lan = useLanguageStore(s => s.lan)
+    const [loading, setLoading] = useState(false)
     const buttonLabel = orderType === 'I' ? `${lan === 'EN' ? 'New Order' : 'Nueva Orden'}` : `${lan === 'EN' ? 'Place Order' : 'Enviar Pedido'}`
+    const buttonLabelLoading = orderType === 'I' ? `${lan === 'EN' ? 'Creating' : 'creando'}` : `${lan === 'EN' ? 'Placing' : 'Enviando'}`
 
     const handleCreateOrder = () => {
+        setLoading(true)
         createOrder.mutate({ order: { created_by: userId, table, status, order_type:orderType
-         }, access })
+         }, access }, {
+            onError: err => {
+              setShow(true)
+              setType('error')
+              setMessage(`Error: ${err}`)
+            },
+            onSettled: () => setLoading(false)
+         })
     }
 
   return (
     <div>
+        {loading 
+        ? 
+        <Button 
+            label={buttonLabelLoading}
+            disable={true}
+        />
+        :
         <Button 
             label={buttonLabel}
             onClick={handleCreateOrder}
         />
+        }
     </div>
   )
 }
